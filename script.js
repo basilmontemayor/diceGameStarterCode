e529576f0e4ca562306fbba8d860f201f7c61fbf
Basil Montemayor <basil.l.montemayor@gmail.com>
2017-02-22 23:29:24 +0000

    dice

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  function rolldice(){
-  //Place your code here 
-
-
-  };
-  
-});
+  $("#roll-dice").click(function(){
+    var firstrandom = (Math.floor(Math.random()*6 + 1));
+    var secondrandom
+    $("#first-die").append("one");
+   }) 
+  });
